6117e17a177a6d3188f7ae002ef5346c3435d03e
Haran840 <haransarwan@gmail.com>
2024-05-27 21:16:42 +0530

    "Step 3 creation and overall optimisation"

--- src/app/third-step/third-step.component.ts
+++ src/app/third-step/third-step.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { SelectedCar } from '../connectors/models/SelectedCar';
+import { PrependDollarPipe } from "../utils/custom-pipes/prepend-dollar.pipe";
+import { ConnectorService } from '../connectors/services/connector.service';
 
 @Component({
-  selector: 'app-third-step',
-  standalone: true,
-  imports: [],
-  templateUrl: './third-step.component.html',
-  styleUrl: './third-step.component.scss'
+    selector: 'app-third-step',
+    standalone: true,
+    templateUrl: './third-step.component.html',
+    styleUrl: './third-step.component.scss',
+    imports: [CommonModule, PrependDollarPipe]
 })
-export class ThirdStepComponent {
+export class ThirdStepComponent implements OnInit{
+  selectedVehicle:SelectedCar=new SelectedCar();
+
+  constructor(private connectorService:ConnectorService){}
+
+  ngOnInit(): void {
+    this.connectorService.selectedCarObservable.subscribe(selectedCar=>
+      this.selectedVehicle=selectedCar
+    );
+  }
 
 }
